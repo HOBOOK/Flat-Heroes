71bf17523cc80b8379b068c49e9d9d9cfb6eacb3
호북이 hobook games <pkh879@gmail.com>
2019-06-18 17:14:19 +0000

    배경 수정

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -11,7 +11,7 @@ public class FollowCamera : MonoBehaviour
     private Vector3 dragOriginPos;
     // 마우스 줌 //
     float zoom;
-    float otrhSize =4.5f;
+    float otrhSize =4f;
     private void Start()
     {
         GetComponent<Camera>().orthographicSize = otrhSize;
@@ -81,7 +81,7 @@ public class FollowCamera : MonoBehaviour
     }
     void Zoom(float increment)
     {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, 4, 5);
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, 3.5f, 4);
     }
     private void MoveCamera()
     {
